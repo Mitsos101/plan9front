a090ec4429781c1e8e27e9e041b5cc00e4270693
Mitsos101 <demetrius.iatrakis@gmail.com>
2021-07-15 12:50:49 +0300

    Update oauth.c

--- sys/src/cmd/auth/factotum/oauth.c
+++ sys/src/cmd/auth/factotum/oauth.c
@@ -1,5 +1,23 @@
 #include "dat.h"
 
+/* webfs and factotum need to be in the same namespace */
+static int
+bindwebfs(void)
+{
+	int srvfd;
+
+	if(access("/mnt/web", AEXIST) < 0){
+		if((srvfd = open("#s/web", ORDWR)) >= 0){
+			if(mount(srvfd, -1, "/mnt", MBEFORE, "") != -1)
+				return 0;
+			close(srvfd);
+		}
+		return -1;
+	}
+	return 0;
+}
+
+
 typedef struct State State;
 struct State
 {
@@ -88,5 +106,5 @@ Proto oauth =
 .read=		oauthread,
 .close=		oauthclose,
 .addkey=		replacekey,
-.keyprompt=	"issuer? scope? client_id?",
+.keyprompt=	"issuer? scope? client_id? !refresh_token?",
 };
