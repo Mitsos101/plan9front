3b1ec10f744db257f6cc5aa1c333ff784ddc3264
Mitsos101 <demetrius.iatrakis@gmail.com>
2021-08-10 17:42:59 +0300

    auth/oauth: add oauth_getkey

--- sys/src/cmd/auth/oauth.c
+++ sys/src/cmd/auth/oauth.c
@@ -9,6 +9,28 @@ usage(void)
 	exits("usage");
 }
 
+int
+oauth_getkey(char *params)
+{
+	Attr *a;
+	char *verification_uri;
+	char *user_code;
+
+	if((a = _parseattr(params)) == nil)
+		return auth_getkey(params);
+
+	 if((verification_uri = _strfindattr(a, "verification_uri")) == nil
+	 || (user_code = _strfindattr(a, "user_code")) == nil){
+	 	_freeattr(a);
+	 	return auth_getkey(params);
+	 }
+
+	 fprint(2, "go to %s\n", verification_uri);
+	 fprint(2, "your code is %s\n", user_code);
+
+	 return 0;
+}
+
 void
 main(int argc, char **argv)
 {
@@ -22,7 +44,7 @@ main(int argc, char **argv)
 	if(argc != 1)
 		usage();
 
-	o = auth_getoauth(auth_getkey, "proto=oauth %s", argv[0]);
+	o = auth_getoauth(oauth_getkey, "proto=oauth %s", argv[0]);
 	if(o == nil)
 		sysfatal("getoauth: %r");
 
